49050c8535abd0efb79950d0bd4383c294f8ecf9
Eric Hendrix <agentmrbig@gmail.com>
2020-04-21 02:02:46 -0400

    Added level items json data file, can drag some items around in level to get x and y values for proper placement and debugging

--- js/main.js
+++ js/main.js
@@ -35,32 +35,12 @@ gameScene.preload = function () {
     margin: 1,
     spacing: 1,
   });
+
+  this.load.json('levelData', 'assets/json/levelData.json');
 };
 
 // executed once, after assets were loaded
 gameScene.create = function () {
-  // world bounds
-  this.physics.world.bounds.width = this.gameWidth;
-  this.physics.world.bounds.height = this.gameHeight;
-
-  this.platforms = this.add.group();
-  // ground
-  let ground = this.add.sprite(180, 400, 'ground');
-  this.physics.add.existing(ground, true);
-  this.platforms.add(ground);
-
-  // platform
-  let platform = this.add.tileSprite(180, 300, 3 * 36, 1 * 30, 'block');
-  this.physics.add.existing(platform, true);
-  this.platforms.add(platform);
-
-  // player
-  this.player = this.add.sprite(180, 250, 'player', 3);
-  this.physics.add.existing(this.player);
-
-  // constrain player to game bounds
-  this.player.body.setCollideWorldBounds(true);
-
   // walking animation
   this.anims.create({
     key: 'walking',
@@ -72,19 +52,35 @@ gameScene.create = function () {
     repeat: -1,
   });
 
-  //ground.body.allowGravity = false;
+  // fire animation
+  this.anims.create({
+    key: 'burning',
+    frames: this.anims.generateFrameNames('fire', {
+      frames: [0, 1],
+    }),
+    frameRate: 4,
+    repeat: -1,
+  });
 
-  // make it immovable
-  //ground.body.immovable = true;
+  // world bounds
+  this.physics.world.bounds.width = this.gameWidth;
+  this.physics.world.bounds.height = this.gameHeight;
 
-  // 2 creating and adding sprites to the physics system at one time
-  //let ground2 = this.physics.add.sprite(180, 200, 'ground');
+  this.platforms = this.add.group();
+
+  // add all level elements
+  this.setupLevel();
 
   // collision detection
   this.physics.add.collider(this.player, this.platforms);
+  this.physics.add.collider(this.goal, this.platforms);
 
   // enable cursor keys
   this.cursors = this.input.keyboard.createCursorKeys();
+
+  this.input.on('pointerdown', function (pointer) {
+    console.log(pointer.x, pointer.y);
+  });
 };
 
 // executed on every frame
@@ -132,6 +128,90 @@ gameScene.update = function () {
     // change the frame
     this.player.setFrame(2);
   }
+};
+
+// sets up all the elements in the level
+gameScene.setupLevel = function () {
+  // load json data
+  this.levelData = this.cache.json.get('levelData');
+
+  // create all the platforms
+  this.platforms = this.add.group();
+  for (let i = 0; i < this.levelData.platforms.length; i++) {
+    let curr = this.levelData.platforms[i];
+
+    let newObj;
+
+    // create object
+    if (curr.numTiles == 1) {
+      // create sprite
+      newObj = this.add.sprite(curr.x, curr.y, curr.key).setOrigin(0, 0);
+    } else {
+      // create tilesprite
+      let width = this.textures.get(curr.key).get(0).width;
+      let height = this.textures.get(curr.key).get(0).height;
+      newObj = this.add
+        .tileSprite(curr.x, curr.y, curr.numTiles * width, height, curr.key)
+        .setOrigin(0);
+    }
+
+    // enable physics
+    this.physics.add.existing(newObj, true);
+
+    // add to the group
+    this.platforms.add(newObj);
+  }
+
+  // create all the fires
+  this.fires = this.add.group();
+  for (let i = 0; i < this.levelData.fires.length; i++) {
+    let curr = this.levelData.fires[i];
+
+    let newObj = this.add.sprite(curr.x, curr.y, 'fire').setOrigin(0);
+
+    // enable physics
+    this.physics.add.existing(newObj);
+    newObj.body.allowGravity = false;
+    newObj.body.imovable = true;
+
+    // play burning animation
+    newObj.anims.play('burning');
+
+    // add to the group
+    this.fires.add(newObj);
+
+    // this is for level creation
+    newObj.setInteractive();
+    this.input.setDraggable(newObj);
+  }
+
+  // for level creation
+  this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+    gameObject.x = dragX;
+    gameObject.y = dragY;
+
+    console.log(dragX, dragY);
+  });
+
+  // player
+  this.player = this.add.sprite(
+    this.levelData.player.x,
+    this.levelData.player.y,
+    'player',
+    3
+  );
+  this.physics.add.existing(this.player);
+
+  // constrain player to game bounds
+  this.player.body.setCollideWorldBounds(true);
+
+  // goal
+  this.goal = this.add.sprite(
+    this.levelData.goal.x,
+    this.levelData.goal.y,
+    'goal'
+  );
+  this.physics.add.existing(this.goal);
 };
 
 // our game's configuration
